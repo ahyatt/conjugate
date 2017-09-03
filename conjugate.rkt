ec7d145c19900bc7edd77ea13312aee85b0c3079
Andrew Hyatt <ahyatt@ahyatt-macbookpro6.roam.corp.google.com>
2017-09-03 00:02:40 -0400

    Fix tty behavior.
    Now we just get one character at a time, and don't echo.

--- conjugate.rkt
+++ conjugate.rkt
@@ -29,13 +29,13 @@
 (define use-irregulars (make-parameter #t))
 (define trials (make-parameter 10))
 
-(define lang (command-line 
+(define lang (command-line
                       #:once-each
                       [("-r" "--regulars-only")
                        "Use only regular verbs" (use-irregulars #f)]
                       [("-t" "--trials") num-trials
                                          "Run <trials> number of trials"
-                                         (trials (num-trials string->number))]
+                                         (trials (string->number num-trials))]
                       #:args (lang)
                       lang))
 
@@ -45,8 +45,25 @@
                 (format "forms-~a" lang)
                 (lambda () (string-split
                             (port->string) #px"\n"))))
-(map (lambda (inf-form) (test-single (car inf-form) (cdr inf-form)))
-     (take (shuffle
-            (map (lambda (l) (cons (first l) (second l)))
-                 (cartesian-product (append inf-reg inf-irreg)
-                                    forms))) (trials)))
+
+(file-stream-buffer-mode (current-input-port) 'none)
+(let* ([stty (find-executable-path "stty")]
+       [starting-stty
+        (let ([s (open-output-string)])
+          (parameterize [(current-output-port s)]
+            (unless (system* stty "-g")
+              (error "Could not get the current stty settings"))
+            (regexp-replace #rx"\r?\n$" (get-output-string s) "")))])
+  (dynamic-wind
+    (lambda ()
+      (unless (system* stty "raw" "-echo" "opost")
+        (error "Could not set the stty settings to raw")))
+    (lambda ()
+      (map (lambda (inf-form) (test-single (car inf-form) (cdr inf-form)))
+           (take (shuffle
+                  (map (lambda (l) (cons (first l) (second l)))
+                       (cartesian-product (append inf-reg inf-irreg)
+                                          forms))) (trials))))
+    (lambda ()
+      (unless (system* stty starting-stty)
+        (error "Could not reset the stty settings")))))
